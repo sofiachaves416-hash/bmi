a37783d565d4d3e3ea829110b35a48f30cf56cd9
sofiachaves416-hash <sofiachaves416@gmail.com>
2026-07-05 23:04:57 -0300

    Modificacion else if

--- bmi.c
+++ bmi.c
@@ -1,24 +1,35 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main(void) {
 	float peso, altura, bmi;
-	
+
 	printf("Ingrese el peso en kg: ");
 	scanf("%f", &peso);
 	
 	printf("Ingrese la altura en metros: ");
 	scanf("%f", &altura);
-	
+
 	bmi = peso / (altura * altura);
-	
+
 	printf("\nSu indice de masa corporal es: %.2f\n\n", bmi);
-	printf("Indice\t\tCondicion\n");
+
+	printf("Resultado de su evaluacion:\n");
 	printf("-------------------------\n");
-	printf("<18.5\t\tBajo peso\n");
-	printf("18.5-24.9\tNormal\n");
-	printf("25.0-29.9\tSobrepeso\n");
-	printf(">=30\t\tObesidad\n");
+	
+	if (bmi < 18.5) {
+		printf("Indice: <18.5\t\tCondicion: Bajo peso\n");
+	} 
+	else if (bmi < 25.0) { 
+		printf("Indice: 18.5-24.9\tCondicion: Normal\n");
+	} 
+	else if (bmi < 30.0) {
+		printf("Indice: 25.0-29.9\tCondicion: Sobrepeso\n");
+	} 
+	else {
+		printf("Indice: >=30\t\tCondicion: Obesidad\n");
+	}
+	
+	printf("-------------------------\n");
 	
 	return 0;
 }
